aaf04b1fa3beebd13165f21414fc082dc34f0488
cheatcode11 <ashishmurthy11@gmail.com>
2025-03-08 17:24:05 -0800

    idk

--- gate_sim/part1_gate_level/xcelium.d/run.lnx8664.20.09.d/.timestamp.ts
+++ gate_sim/part1_gate_level/xcelium.d/run.lnx8664.20.09.d/.timestamp.ts
@@ -1,3 +1,5 @@
 1737007846 /home/linux/ieng6/ee260bwi25/public/PDKdata/verilog/tcbn65gplus.v
+1741482198 /home/linux/ieng6/ee260bwi25/a2murthy/ECE260B_course_project/gate_sim/part1_gate_level/netlist/fullchip_tb.v
 1741474646 /home/linux/ieng6/ee260bwi25/a2murthy/ECE260B_course_project/PnR_Synthesis/ece260_project_Part1_gate-level_sim/netlist/fullchip_tb.v
+1741482198 /home/linux/ieng6/ee260bwi25/a2murthy/ECE260B_course_project/gate_sim/part1_gate_level/netlist/fullchip.pnr.v
 1741422511 /home/linux/ieng6/ee260bwi25/a2murthy/ECE260B_course_project/PnR_Synthesis/ece260_project_Part1_gate-level_sim/netlist/fullchip.pnr.v
